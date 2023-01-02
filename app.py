89c54cc44df247aad8cf51d62d7dc340798ed948
Anurag Dubey <71247280+Anuragd275@users.noreply.github.com>
2023-01-02 13:18:36 +0530

    :wheelchair:
    * wrapped everything in main function

--- app.py
+++ app.py
@@ -1,18 +1,24 @@
 from telethon import TelegramClient
 
-api_id = API_ID
+api_id = 'API_ID' #without ''
 api_hash = 'API_HASH'
-# values of your account
+# values from your account
 client = TelegramClient('session', api_id, api_hash)
 
-async def main():
-    source = ''
-    destination = ''
-    message_id_list = []
+def main():
+    async def forward_message():
+        source = '' 
+        destination = ''
+        #do not play with message_id_list.
+        message_id_list = []
 
-    async for message in client.iter_messages(source, limit=3):
-        message_id_list.append(message.id)
-    await client.send_message(destination, message, source)
+        # limit = 3 will scrape only last 3 messages from the source channel, set its value according to your needs.
+        async for message in client.iter_messages(source, limit=3):
+            message_id_list.append(message.id)
+        await client.send_message(destination, message, source)
 
-with client:
-    client.loop.run_until_complete(main())
+    with client:
+        client.loop.run_until_complete(forward_message())
+
+if __name__ == "__main__":
+    main()
