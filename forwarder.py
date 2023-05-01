50ae4fa751ce8309478c0ef84a6a720a85357af2
Anurag Dubey <71247280+Anuragd275@users.noreply.github.com>
2023-05-01 21:34:58 +0530

    :hammer:
    - added meaningful comments

--- forwarder.py
+++ forwarder.py
@@ -1,17 +1,23 @@
-# Forward messages from 1 channel to another channel 
+"""
+    this python script is designed to forward all messages from a particular channel (source) to another channel (destination).
+    ==> Keep in mind that the forwared message in the destination channel will have the tag saying "forwarded from xyz" channel, where xyz is the source channel.
+"""
 
 from telethon import TelegramClient
 
 api_id = 'API_ID' #without ''
 api_hash = 'API_HASH'
-# values from your account
+# Use your own values from Core Telegram.
 client = TelegramClient('session', api_id, api_hash)
 
 def main():
     async def forward_message():
+        # source channel username/link.
         source = '' 
+        #destination channel username/link.
         destination = ''
-        #do not play with message_id_list.
+
+        #do not play with message_id_list, it may lead to unknown behavior.
         message_id_list = []
 
         # limit = 3 will scrape only last 3 messages from the source channel, set its value according to your needs.
